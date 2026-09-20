aefb8adbf029aacd48a65a12a0ac4b5b27ccd194
CrowRules Entertainment <crowrulesentertainment@gmail.com>
2026-09-20 04:48:52 -0700

    Connect CrowRules+ to Supabase

---
+++ js/supabase-client.js
@@ -0,0 +1,7 @@
+/* CrowRules+ Supabase client. Public publishable key only. */
+(function(){
+"use strict";
+const url="https://cevylpnoexugwgygvtgu.supabase.co";
+const key="sb_publishable_AdfM5y6RqvF3tbvEVzDZSg_JuGTQLD-";
+if(window.supabase&&!window.CROW_SUPABASE)window.CROW_SUPABASE=window.supabase.createClient(url,key);
+})();
